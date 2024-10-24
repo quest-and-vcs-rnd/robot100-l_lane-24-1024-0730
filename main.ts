a145f2d3bfcd97a8dd932c4b9a15c3af96977255
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-10-24 07:36:38 -0700

    24-1024-0740 First Draft

--- main.ts
+++ main.ts
@@ -1,6 +1,41 @@
-basic.showIcon(IconNames.Heart)
-quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    50,
+    50
+    )
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        # # # # #
+        . # . # .
+        `)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(10, quest_Time_Units_Enum.Seconds)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    0
+    )
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
+basic.showIcon(IconNames.Happy)
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+0,
+0
+)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
